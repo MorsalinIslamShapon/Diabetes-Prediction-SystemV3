5b6c56c6759f0c889c2d0485936ab4c94aa7b387
Shankhadweep Hazra <hazrashankhadweep17@gmail.com>
2025-06-16 01:36:43 +0530

    improved accuracy

--- function/model.py
+++ function/model.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from function.transformers import ColumnSelector
 
 
@@ -8,10 +9,28 @@ selected_columns = [
     'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'
 ]
 
-# Pipeline setup
-Model = Pipeline([
+# Define the pipeline
+base_pipeline = Pipeline([
     ('column_selector', ColumnSelector(selected_columns)),
-    ('model', RandomForestClassifier(max_depth=6,
-                                     n_estimators=300,
-                                     criterion='entropy'))
+    ('model', RandomForestClassifier(random_state=42))
 ])
+
+# Define hyperparameter grid
+param_grid = {
+    'model__n_estimators': [200, 300, 400, 500],
+    'model__max_depth': [6, 8, 10, 12],
+    'model__min_samples_split': [2, 5, 10],
+    'model__min_samples_leaf': [1, 2, 4],
+    'model__criterion': ['gini', 'entropy'],
+    'model__max_features': ['sqrt', 'log2']
+}
+
+# Create GridSearchCV object
+Model = GridSearchCV(
+    estimator=base_pipeline,
+    param_grid=param_grid,
+    cv=5,
+    scoring='roc_auc',
+    n_jobs=-1,
+    verbose=1
+)
--- training.py
+++ training.py
@@ -7,11 +7,15 @@ data = pd.read_csv('datasets/diabetes.csv')
 X = data[['Pregnancies', 'Glucose','BloodPressure','SkinThickness', 'Insulin', 'BMI','DiabetesPedigreeFunction', 'Age']]
 y = data['Outcome']
 
-
+# Fit the model with grid search
 Model.fit(X, y)
 
+# Get the best model's predictions
 y_pred = Model.predict_proba(X)[:, 1]
 
-print("ROC_AUC Score: ", (roc_auc_score(y, y_pred) * 100).round(2))
+# Print the results
+print("Best parameters:", Model.best_params_)
+print("Best ROC-AUC Score: ", (roc_auc_score(y, y_pred) * 100).round(2))
 
-joblib.dump(Model, 'model.pkl')
+# Save the best model
+joblib.dump(Model.best_estimator_, 'model.pkl')
